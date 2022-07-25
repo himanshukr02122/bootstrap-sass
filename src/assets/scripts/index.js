7f6fd426a1fbb433b6a38d3a90f4f00bfde8296e
Himanshu Kumar <himanshukumar02122@gmail.com>
2022-07-25 23:21:04 +0530

    update

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -8,12 +8,34 @@ $(()=>{
       $('#footer-minus-icon').show();
       $('#ul-toggle').show();
     });
-
     $('#footer-minus-icon').on('click',()=> {
       $('#footer-minus-icon').hide();
       $('#footer-plus-icon').show();
       $('#ul-toggle').hide();
     });
+
+    $('#footer-plus-icon1').on('click',()=> {
+      $('#footer-plus-icon1').hide();
+      $('#footer-minus-icon1').show();
+      $('#ul-toggle1').show();
+    });
+    $('#footer-minus-icon1').on('click',()=> {
+      $('#footer-minus-icon1').hide();
+      $('#footer-plus-icon1').show();
+      $('#ul-toggle1').hide();
+    });
+
+    $('#footer-plus-icon2').on('click',()=> {
+      $('#footer-plus-icon2').hide();
+      $('#footer-minus-icon2').show();
+      $('#ul-toggle2').show();
+    });
+    $('#footer-minus-icon2').on('click',()=> {
+      $('#footer-minus-icon2').hide();
+      $('#footer-plus-icon2').show();
+      $('#ul-toggle2').hide();
+    });
+
     var x = window.matchMedia("(min-width: 768px)")
     x.addListener(()=> {
     if (x.matches) { // If media query matches
